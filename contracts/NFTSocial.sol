3883e0fb8f8b4178af19fd4754c8f528442e2ce7
daenamcclintock <daena.mcclintock@gmail.com>
2022-07-21 09:10:54 -0400

    added events

--- contracts/NFTSocial.sol
+++ contracts/NFTSocial.sol
@@ -3,7 +3,10 @@ pragma solidity ^0.8.9;
 
 contract NFTSocial {
 
-
+    event PostCreated (bytes32 indexed postId, address indexed postOwner, bytes32 indexed parentId, bytes32 contentId, bytes32 categoryId);
+    event ContentAdded (bytes32 indexed contentId, string contentUri);
+    event CategoryCreated (bytes32 indexed categoryId, string category);
+    event Voted (bytes32 indexed postId, address indexed postOwner, address indexed voter, uint80 reputationPostOwner, uint80 reputationVoter, int40 postVotes, bool up, uint8 reputationAmount);
 
     // Data structure for each post
     struct post {
