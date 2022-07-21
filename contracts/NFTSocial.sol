a0ea5a918ef75e13c7d6b83a293daa30c92171ef
daenamcclintock <daena.mcclintock@gmail.com>
2022-07-21 08:42:43 -0400

    added post struct data structure

--- contracts/NFTSocial.sol
+++ contracts/NFTSocial.sol
@@ -2,5 +2,15 @@
 pragma solidity ^0.8.9;
 
 contract NFTSocial {
+
     
+
+    // Data structure for each post
+    struct post {
+        address postOwner;
+        bytes32 parentPost; // used to implement comments as a child of each post
+        bytes32 contentId;
+        int40 votes;
+        bytes32 categoryId;
+    }
 }
