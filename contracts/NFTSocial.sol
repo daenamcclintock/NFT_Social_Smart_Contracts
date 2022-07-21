4cdd2d15eebfb75f2921ce849265445226af7fa6
daenamcclintock <daena.mcclintock@gmail.com>
2022-07-21 08:50:14 -0400

    added mapping variables

--- contracts/NFTSocial.sol
+++ contracts/NFTSocial.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.9;
 
 contract NFTSocial {
 
-    
+
 
     // Data structure for each post
     struct post {
@@ -13,4 +13,12 @@ contract NFTSocial {
         int40 votes;
         bytes32 categoryId;
     }
+
+    mapping (address => mapping (bytes32 => uint80)) reputationRegistry; // mapping user address to a mapping of categoryId to category name (categoryRegistry)
+    mapping (bytes32 => string) categoryRegistry; // mapping categoryId to the category name
+    mapping (bytes32 => string) contentRegistry; // mapping the contentId to the url in IPFS
+    mapping (bytes32 => post) postRegistry; // mapping the postId to the post data structure
+    mapping (address => mapping (bytes32 => bool)) voteRegistry; // mapping user address to a mapping of voteId to a boolean (like/dislike => true/false)
+
+    
 }
