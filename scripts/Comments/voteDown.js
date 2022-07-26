45a8b438f40ecb22d791dc00c7624bf0412b3472
daenamcclintock <daena.mcclintock@gmail.com>
2022-07-26 18:13:36 -0400

    added voteDown script

---
+++ scripts/Comments/voteDown.js
@@ -0,0 +1,23 @@
+const { ethers, network } = require("hardhat")
+const { moveBlocks } = require("../../utils/move-blocks")
+
+const postId = "0x0000000000000000000000000000000000000000000000000000000000000000"
+const reputationAdded = 1
+
+const voteDown = async () => {
+    const comments = await ethers.getContract("Comments")
+    const transaction = await comments.voteDown(postId, reputationAdded)
+    await transaction.wait(1)
+    console.log("Post Disliked !")
+    if (network.config.chainId == "31337") {
+        await moveBlocks(2, (sleepAmount = 1000))
+    }
+}
+
+
+voteDown()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
